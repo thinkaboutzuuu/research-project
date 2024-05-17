f81d3bdff0037af73748f68cd7bacf8376e870d7
alvinkwon1006 <alvinkwon1006@gmail.com>
2024-05-17 00:03:04 -0400

    fixed :)

--- version2/DATA/get_another_crypto.py
+++ version2/DATA/get_another_crypto.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import pandas_ta as ta
-import yfinance as yf
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from datetime import datetime, timedelta
 
 # Sample crypto currencies
@@ -21,10 +18,12 @@ cryptos = [
 ]
 
 # Generating sample data for 30 days
-start = datetime(2010, 1, 1)
+start = datetime(2000, 1, 1)
+end = datetime(2023, 12, 31)
+days = (end - start).days + 1
 for crypto, filename in cryptos:
     data = []
-    for i in range(30):
+    for i in range(days):
         date = start + timedelta(days = i)
         open = round(100 + 10 * i + (i % 10) * 0.1, 2)
         high = open + round((i % 5) * 0.2, 2)
@@ -32,15 +31,14 @@ for crypto, filename in cryptos:
         close = round(open + (high - low) * 0.5, 2)
         volume = 1000000 + i * 10000
 
-        data.append(date, crypto, open, high, low, close, volume)
+        data.append([date, crypto, open, high, low, close, volume])
 
     # Creating a dataframe
     dataframe = pd.DataFrame(data, columns = ['date', 'crypto', 'open', 
                                               'high', 'low', 'close', 'volume'])
-    df.to_csv(filename, index = False)
+    dataframe.to_csv(filename, index = False)
 '''
-Side notes:
-cd DATA
-python get_crypto.py
-which (module)
+path:
+cd /Users/alvinkwon/Documents/GitHub/research-project/version2/DATA
+python get_another_crypto.py
 '''
